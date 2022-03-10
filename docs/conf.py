c0e8b50e094626cd9ad582616228adbc8cf57458
Nicolas Rouanet <nicolas.rouanet@fullsave.com>
2022-03-10 22:59:41 +0100

    Welcome to 2022! :D

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Traffic Info"
-copyright = "2019, Nicolas Rouanet"
+copyright = "2022, Nicolas Rouanet"
 author = "Nicolas Rouanet"
 
 # The short X.Y version
